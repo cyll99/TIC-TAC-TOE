330e9f921a9a9316a72796de3e3dd74a476d9100
CyllX <christyanlove@gmail.com>
2021-08-17 17:35:50 -0500

    little change with poo

--- version2.0.py
+++ version2.0.py
@@ -4,134 +4,108 @@ import random
 coord = {1: '00', 2: '01', 3: '02', 4: '10', 5: '11', 6: '12', 7: '20', 8: '21', 9: '22'}
 
 
+def end_game(board):  # check if there's 0 left in the board
+    if True in np.array(np.any(board == 0)):
+        return True
+
+
+def player1_status(board):
+    result1 = np.all(board == 1, axis=0)
+    result2 = np.all(board == 1, axis=1)
+    cross1 = np.array([int(board[0, 0]), int(board[1, 1]), int(board[2, 2])])
+    cross2 = np.array([int(board[0, 2]), int(board[1, 1]), int(board[2, 0])])
+    if True in result1 or True in result2 or np.all(cross1 == 1) == True or np.all(cross2 == 1) == True:
+        return True
+
+
+def player2_status(board):
+    result1 = np.all(board == 2, axis=0)
+    result2 = np.all(board == 2, axis=1)
+    cross1 = np.array([int(board[0, 0]), int(board[1, 1]), int(board[2, 2])])
+    cross2 = np.array([int(board[0, 2]), int(board[1, 1]), int(board[2, 0])])
+    if True in result1 or True in result2 or np.all(cross1 == 2) == True or np.all(cross2 == 2) == True:
+        return True
+
+
 def verify_digit(n):
     while not n.isdigit and len(n) > 1 and int(n) not in range(1, 10):
         n = input("Enter a digit please:\n ")
     return int(n)
 
 
-class Player1(object):
-    def __init__(self, name):
-        self.name = name
-
-    def enter_info(self):
-        name = input("Player1's name: ")
-        self.name = name
-
-    def player_status(self, board):
-        result1 = np.all(board == 1, axis=0)
-        result2 = np.all(board == 1, axis=1)
-        cross1 = np.array([int(board[0, 0]), int(board[1, 1]), int(board[2, 2])])
-        cross2 = np.array([int(board[0, 2]), int(board[1, 1]), int(board[2, 0])])
-        if True in result1 or True in result2 or np.all(cross1 == 1) == True or np.all(cross2 == 1) == True:
-            return True
-
-    def player_move(self, board):
+def player1_move(board):
+    num = input()
+    num = verify_digit(num)
+    nb = coord[num]
+    while board[int(nb[0]), int(nb[1])] > 0:
+        print('Take another move please!!')
         num = input()
         num = verify_digit(num)
         nb = coord[num]
-        while board[int(nb[0]), int(nb[1])] > 0:
-            print('Take another move please!!')
-            num = input()
-            num = verify_digit(num)
-            nb = coord[num]
-        board[int(nb[0]), int(nb[1])] = 1
-        print(board)
+    board[int(nb[0]), int(nb[1])] = 1
+    print(board)
 
 
-class Player2(Player1):
-    def __init__(self, name):
-        super().__init__(name)
-
-
-    def enter_info(self):
-        name = input("Player2's name: ")
-        self.name = name
-
-    def player_status(self, board):
-        result1 = np.all(board == 2, axis=0)
-        result2 = np.all(board == 2, axis=1)
-        cross1 = np.array([int(board[0, 0]), int(board[1, 1]), int(board[2, 2])])
-        cross2 = np.array([int(board[0, 2]), int(board[1, 1]), int(board[2, 0])])
-        if True in result1 or True in result2 or np.all(cross1 == 2) == True or np.all(cross2 == 2) == True:
-            return True
-
-    def player_move(self, board):
+def player2_move(board):
+    num = input()
+    num = verify_digit(num)
+    nb = coord[num]
+    while board[int(nb[0]), int(nb[1])] > 0:
+        print('Take another move please!!')
         num = input()
         num = verify_digit(num)
         nb = coord[num]
-        while board[int(nb[0]), int(nb[1])] > 0:
-            print('Take another move please!!')
-            num = input()
-            num = verify_digit(num)
-            nb = coord[num]
-        board[int(nb[0]), int(nb[1])] = 2
-        print(board)
+    board[int(nb[0]), int(nb[1])] = 2
+    print(board)
 
 
-class Computer(Player1):
-    def __init__(self, name):
-        super().__init__(name)
-
-    def computer_move(self, board):
+def computer_move(board):
+    a = random.randint(0, 2)
+    b = random.randint(0, 2)
+    while board[a, b] > 0:
         a = random.randint(0, 2)
         b = random.randint(0, 2)
-        while board[a, b] > 0:
-            a = random.randint(0, 2)
-            b = random.randint(0, 2)
-        board[a, b] = 2
-        print(board)
-
-
-player1 = Player1('Player1')
-player2 = Player2('Player2')
-computer = Computer('Computer')
-
-
-def end_game(board):  # check if there's 0 left in the board
-    if True in np.array(np.any(board == 0)):
-        return True
+    board[a, b] = 2
+    print(board)
 
 
 def play_with_comp():
     board = np.zeros((3, 3))
     print('START!!')
     while end_game(board):
-        Player2.player_move(player2, board)
+        player1_move(board)
         if not end_game(board):
             print('GAME OVER!!')
             break
-        if Player2.player_status(player2, board):
-            print(f'{player2.name} WON :)')
+        if player1_status(board):
+            print('YOU WON :)')
             break
-        Computer.computer_move(computer, board)
+        computer_move(board)
         if not end_game(board):
             print('GAME OVER!!')
             break
-        if Computer.player_status(computer, board):
+        if player2_status(board):
             print('YOU LOST ):')
             break
 
 
 def play_with_others():
-    Player1.enter_info(player1)
-    Player2.enter_info(player2)
     board = np.zeros((3, 3))
     print('START!!')
     while end_game(board):
-        Player1.player_move(player1, board)
+        player1_move(board)
         if not end_game(board):
             print('GAME OVER!!')
             break
-        if Player1.player_status(player1, board):
-            print(f'{player1.name} WON :)')
+        if player1_status(board):
+            print('YOU WON :)')
             break
-        Player2.player_move(player2, board)
+        player2_move(board)
         if not end_game(board):
             print('GAME OVER!!')
             break
-        if Player2.player_status(player2, board):
-            print(f'{player2.name} WON :)')
+        if player2_status(board):
+            print('YOU LOST ):')
             break
 
 
